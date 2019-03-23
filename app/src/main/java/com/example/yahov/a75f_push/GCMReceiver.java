759d8045bf56ce0bba4bc26c4549a5af94ed54bf
bilger-progress <bilger.yahov@progress.com>
2019-03-23 19:56:35 +0200

    Implement a Receiver and a Service for the Push Notifications.

---
+++ app/src/main/java/com/example/yahov/a75f_push/GCMReceiver.java
@@ -0,0 +1,15 @@
+package com.example.yahov.a75f_push;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class GCMReceiver extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ComponentName comp = new ComponentName(context.getPackageName(), GCMService.class.getName());
+        GCMService.enqueueWork(context, (intent.setComponent(comp)), GCMService.class);
+    }
+}
---
+++ app/src/main/java/com/example/yahov/a75f_push/GCMService.java
@@ -0,0 +1,83 @@
+package com.example.yahov.a75f_push;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import com.kinvey.android.push.KinveyGCMService;
+
+public class GCMService extends KinveyGCMService {
+
+    @Override
+    public void onMessage(String message) {
+        displayNotification(message);
+    }
+
+    @Override
+    public void onError(String error) {
+        displayNotification(error);
+    }
+
+    @Override
+    public void onDelete(String deleted) {
+        displayNotification(deleted);
+    }
+
+    @Override
+    public void onRegistered(String gcmID) {
+        displayNotification(gcmID);
+    }
+
+    @Override
+    public void onUnregistered(String oldID) {
+        displayNotification(oldID);
+    }
+
+    public Class getReceiver() {
+        return GCMReceiver.class;
+    }
+
+    private void displayNotification(String message){
+        int NOTIFICATION_ID = 1;
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = null;
+
+        // When you target Android 8.0 (API level 26), you must implement one or more notification channels to display notifications to your users.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String CHANNEL_ID = "my_channel_01";
+            CharSequence name = "my_channel";
+            String Description = "This is my channel.";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            notificationManager.createNotificationChannel(mChannel);
+            builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle(getApplicationContext().getResources().getString(R.string.app_name))
+                    .setContentText(message);
+        } else {
+            builder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle(getApplicationContext().getResources().getString(R.string.app_name))
+                    .setContentText(message);
+        }
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultPendingIntent);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+}
